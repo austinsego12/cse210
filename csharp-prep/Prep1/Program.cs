0c0d309825045801b5db929065e11d0acf8daa4e
austinsego12 <dragoastee123@gmail.com>
2026-04-24 12:18:19 -0500

    fixed intentation on program 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,12 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-string firstName = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
-Console.Write("What is your last name? ");
-string lastName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
 
-Console.WriteLine();
-Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
